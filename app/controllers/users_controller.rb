9b67e460e4e2f75b0106a86c81e053d7b6256194
M.Shehata <shehata.mohammed@gmail.com>
2012-05-09 01:17:21 +0200

    c

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
   def index
   end
+  
   def new
     @user = User.new
+  end
+  
+  def create
+    @user = User.new params[:user]
+    @user.save
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 DsGame::Application.routes.draw do
   root :to =>"users#new"
+  resources :users
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
