6faaf5ba6f4c2fd702c9b804d0e697839a1c0ece
M.Shehata <shehata.mohammed@gmail.com>
2012-05-08 22:17:19 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DsGame::Application.config.secret_token = '46d25af8c929072a1f0e949a9836b3407bac102b4e062f54eec25059910971fe52c2b639e168e9d0c1af7b255308d33e94238a1a5f22d14fba9156aa0231e7f7'
