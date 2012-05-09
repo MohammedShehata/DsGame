1d99d0e60aafe2d22c720abb319adfe0f0cc0c87
M.Shehata <shehata.mohammed@gmail.com>
2012-05-09 02:56:36 +0200

    cc

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
-  belongs_to :user1, :class_name => "User", :foreign_key => "user_id"
-  belongs_to :user2, :class_name => "User", :foreign_key => "user_id"
-  belongs_to :turn, :class_name => "User", :foreign_key => "user_id"
+  belongs_to :user1, :class_name => "User", :foreign_key => "user1_id"
+  belongs_to :user2, :class_name => "User", :foreign_key => "user2_id"
+  belongs_to :turn, :class_name => "User", :foreign_key => "turn"
   has_one :board
 end
